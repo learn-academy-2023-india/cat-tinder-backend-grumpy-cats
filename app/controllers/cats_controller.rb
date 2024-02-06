248cb047d4581953b7e106c1c7441644a8229b80
Mamba240824 <leo.ramos70@yahoo.com>
2024-02-06 11:36:32 -0800

    add model validations for cat and request specs for 422 errors

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -14,8 +14,23 @@ class CatsController < ApplicationController
         if cat.valid?
           render json: cat
         else
-          render json: cat.errors
+          render json: cat.errors, status: 422
         end
+      end
+
+      def update
+        cat = Cat.find(params[:id])
+        cat.update(cat_params)
+        if cat.valid?
+          render json: cat
+        else
+          render json: cat.errors, status: 422
+        end
+      end
+    
+      def destroy
+        cat = Cat.find(params[:id])
+        cat.destroy
       end
     
       private
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -44,4 +44,98 @@ describe "POST /create" do
     # Assure that the created cat has the correct attributes
     expect(cat.name).to eq 'Buster'
   end
+
+  it 'returns a 422 status code' do
+    invalid_cat_params = {
+      cat: {
+        name: nil,
+        age: nil,
+        enjoys: nil,
+        image: nil
+      }
+    }
+
+    post '/cats', params: invalid_cat_params
+    expect(response).to have_http_status(422)
+  end
+
+  describe "PATCH /update" do
+    it "updates a cat" do
+      cat_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      # create a cat
+      post '/cats', params: cat_params
+  
+      # updated cat params
+      updated_cat_params = {
+        cat: {
+          name: 'Buster Brown',
+          age: 6,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      cat = Cat.first
+      # make a request to the cat update endpoint
+      patch "/cats/#{cat.id}", params: updated_cat_params
+
+      expect(response).to have_http_status(200)
+  
+      updated_cat = Cat.first
+      
+      expect(updated_cat.name).to eq('Buster Brown')
+      expect(updated_cat.age).to eq(6)
+      expect(updated_cat.enjoys).to eq('Meow Mix, and plenty of sunshine.')
+      expect(updated_cat.image).to eq('https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80')
+    end
+    it 'returns a 422 status code' do
+      cat_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      # create a cat
+      post '/cats', params: cat_params
+
+      invalid_cat_params = {
+        cat: {
+          name: nil,
+          age: nil,
+          enjoys: nil,
+          image: nil
+        }
+      }
+
+      cat = Cat.first
+      patch "/cats/#{cat.id}", params: invalid_cat_params
+
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deletes a cat' do
+      cat = Cat.create(
+        name: 'Felix',
+        age: 2,
+        enjoys: 'Walks in the park',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+      )
+
+      # make a request to the cat delete endpoint
+      delete "/cats/#{cat.id}"
+
+      expect(response).to have_http_status(:no_content)
+      expect(Cat.find_by(id: cat.id)).to be_nil
+    end
+  end
 end
